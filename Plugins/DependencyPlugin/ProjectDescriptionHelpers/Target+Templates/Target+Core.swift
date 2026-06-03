d3f36f21eb81ba741e8a79be64fcb225dcfc5913
kanghun1121 <kanghun1121@naver.com>
2026-06-03 18:50:27 +0900

    🔥 refactor: Core Example 타겟 제거
    - Target+Core.swift에서 core(example:) 팩토리 제거
    - Core/Project.swift에서 example 타겟 및 CoreExample 스킴 제거
    - Core/Example/ 디렉토리 삭제
    - setup-worktree 스킬에서 Core/Example/Secrets.xcconfig 복사 제거

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Target+Templates/Target+Core.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Target+Templates/Target+Core.swift
@@ -9,12 +9,5 @@ public extension Target {
         return make(factory: f)
     }
 
-    /// Core Example 데모 앱
-    static func core(example factory: TargetFactory) -> Self {
-        var f = factory
-        f.name = "\(ModulePath.Core.name)Example"
-        f.sources = .exampleSources
-        f.product = .app
-        return make(factory: f)
-    }
+
 }
