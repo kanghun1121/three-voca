597dd348e201a26b749b54ca778e48b7d4be84eb
kanghun1121 <kanghun1121@naver.com>
2026-06-04 22:53:46 +0900

    ✨ feat: AudioPlayerClient 추가 및 WordDetail 재생 연결

---
+++ Projects/Domain/Interface/Client/AudioPlayerClient.swift
@@ -0,0 +1,27 @@
+import Dependencies
+import Foundation
+
+public struct AudioPlayerClient: Sendable {
+    public var play: @Sendable (_ url: URL) async -> Void
+
+    public init(play: @escaping @Sendable (_ url: URL) async -> Void) {
+        self.play = play
+    }
+}
+
+extension AudioPlayerClient: TestDependencyKey {
+    public static let testValue = AudioPlayerClient(
+        play: unimplemented("\(Self.self).play", placeholder: ())
+    )
+
+    public static let previewValue = AudioPlayerClient(
+        play: { _ in }
+    )
+}
+
+public extension DependencyValues {
+    var audioPlayerClient: AudioPlayerClient {
+        get { self[AudioPlayerClient.self] }
+        set { self[AudioPlayerClient.self] = newValue }
+    }
+}
