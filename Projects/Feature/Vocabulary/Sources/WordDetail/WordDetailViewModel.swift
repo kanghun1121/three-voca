b20bc8b3e94fa84383583e057f65ad71b51b0603
kanghun1121 <kanghun1121@naver.com>
2026-06-04 22:57:14 +0900

    ♻️ refactor: playAudio를 pronunciationButtonTapped으로 이름 변경

--- Projects/Feature/Vocabulary/Sources/WordDetail/WordDetailViewModel.swift
+++ Projects/Feature/Vocabulary/Sources/WordDetail/WordDetailViewModel.swift
@@ -25,7 +25,7 @@ public final class WordDetailViewModel {
         self.currentIndex = initialIndex
     }
 
-    func playAudio(term: String) async {
+    func pronunciationButtonTapped(term: String) async {
         guard let url = await audioClient.audioURL(term) else { return }
         await audioPlayerClient.play(url)
     }
