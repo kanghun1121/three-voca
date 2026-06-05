7747cfc9d2dc9d52ba96e8565a4b24cffb430ea9
kanghun1121 <kanghun1121@naver.com>
2026-06-06 02:31:25 +0900

    ✨ feat: HTTPClient에 requestData 추가 및 MP3 다운로드에 적용

--- Projects/Core/Sources/Network/HTTPClienting.swift
+++ Projects/Core/Sources/Network/HTTPClienting.swift
@@ -3,4 +3,5 @@ import Foundation
 public protocol HTTPClienting {
     func request<T: Decodable>(_ requestable: any Requestable) async throws -> T
     func request(_ requestable: any Requestable) async throws
+    func requestData(_ requestable: any Requestable) async throws -> Data
 }
