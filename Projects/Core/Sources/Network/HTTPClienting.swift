b90ee32b4bda5aab547345c5d6b613bb2686312f
kanghun1121 <kanghun1121@naver.com>
2026-06-06 02:48:06 +0900

    ⏪️ revert: requestData 제거, URLSession으로 MP3 다운로드 복원

--- Projects/Core/Sources/Network/HTTPClienting.swift
+++ Projects/Core/Sources/Network/HTTPClienting.swift
@@ -3,5 +3,4 @@ import Foundation
 public protocol HTTPClienting {
     func request<T: Decodable>(_ requestable: any Requestable) async throws -> T
     func request(_ requestable: any Requestable) async throws
-    func requestData(_ requestable: any Requestable) async throws -> Data
 }
