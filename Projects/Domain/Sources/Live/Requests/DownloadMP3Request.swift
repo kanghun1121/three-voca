7ce70c5359280418ca68827fb9fc39d5bc231c64
kanghun1121 <kanghun1121@naver.com>
2026-06-06 02:44:12 +0900

    🐛 fix: DownloadMP3Request에서 trailing slash로 인한 403 수정

--- Projects/Domain/Sources/Live/Requests/DownloadMP3Request.swift
+++ Projects/Domain/Sources/Live/Requests/DownloadMP3Request.swift
@@ -6,4 +6,9 @@ struct DownloadMP3Request: Requestable {
     var baseURL: URL { url }
     var path: String { "" }
     var method: HTTPMethod { .get }
+
+    // appendingPathComponent("") 가 trailing slash를 붙이므로 URL을 그대로 사용한다.
+    func makeURLRequest() throws -> URLRequest {
+        URLRequest(url: url)
+    }
 }
