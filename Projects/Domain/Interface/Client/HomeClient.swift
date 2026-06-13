59893dd6c194fce435b74211971721f2062d8d89
kanghun1121 <kanghun1121@naver.com>
2026-06-13 16:10:05 +0900

    ✨ feat: get_heatmap_data RPC 엔드포인트 클라이언트 구성
    - DailyActivity 도메인 모델 추가 (DomainInterface)
    - HeatmapResponseDTO + toDomain() 매핑 추가 (Domain)
    - GetHeatmapDataRequest (POST /rest/v1/rpc/get_heatmap_data) 추가
    - HomeClient에 fetchHeatmapData 클로저 추가 (liveValue / testValue / previewValue)

--- Projects/Domain/Interface/Client/HomeClient.swift
+++ Projects/Domain/Interface/Client/HomeClient.swift
@@ -4,19 +4,26 @@ import Dependencies
 
 public struct HomeClient: Sendable {
     public var fetchHomeOverview: @Sendable () async throws -> VocabularyLibrary
+    public var fetchHeatmapData: @Sendable () async throws -> [DailyActivity]
 
-    public init(fetchHomeOverview: @escaping @Sendable () async throws -> VocabularyLibrary) {
+    public init(
+        fetchHomeOverview: @escaping @Sendable () async throws -> VocabularyLibrary,
+        fetchHeatmapData: @escaping @Sendable () async throws -> [DailyActivity]
+    ) {
         self.fetchHomeOverview = fetchHomeOverview
+        self.fetchHeatmapData = fetchHeatmapData
     }
 }
 
 extension HomeClient: TestDependencyKey {
     public static let testValue = HomeClient(
-        fetchHomeOverview: unimplemented("\(Self.self).fetchHomeOverview")
+        fetchHomeOverview: unimplemented("\(Self.self).fetchHomeOverview"),
+        fetchHeatmapData: unimplemented("\(Self.self).fetchHeatmapData")
     )
 
     public static let previewValue = HomeClient(
-        fetchHomeOverview: { .previewFixture }
+        fetchHomeOverview: { .previewFixture },
+        fetchHeatmapData: { DailyActivity.previewFixture }
     )
 }
 
