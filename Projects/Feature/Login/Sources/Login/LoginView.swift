82b8898341bd3452afd4c4158428a76fb34da2e2
kanghun1121 <kanghun1121@naver.com>
2026-06-26 02:34:42 +0900

    🐛 fix: 화면 배경색 시스템 컬러에서 bg로 통일

--- Projects/Feature/Login/Sources/Login/LoginView.swift
+++ Projects/Feature/Login/Sources/Login/LoginView.swift
@@ -16,7 +16,7 @@ public struct LoginView: View {
             LoginContentView(viewModel: viewModel)
         }
         .ignoresSafeArea(edges: .top)
-        .background(DesignSystemAsset.white.swiftUIColor)
+        .background(DesignSystemAsset.bg.swiftUIColor)
         .sheet(isPresented: $viewModel.isTermsPresented) {
             Text("이용약관")
         }
